81aceaec112f079b63bf21d2ba438f36f874128f
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-09-11 23:24:50 +0200

    Added event application

--- javascript/javascript1/week2/eventapplication.js
+++ javascript/javascript1/week2/eventapplication.js
@@ -5,7 +5,7 @@ function getEventWeekday(day) {
     const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const todaysDayAndDate = week[today.getDay()] + ' ' + today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
     const remainder = day % 7;
-    const eventDay = (today.getDay() + (day % 7)) % 7;
+    const eventDay = (today.getDay() + remainder) % 7;
     return (`Today is ${todaysDayAndDate} and the event is in ${remainder} days. Therefore the event will be held on a ${week[eventDay]}`);
 }
 console.log(getEventWeekday(24));
