af978ebd6766a3532a5a1d31b9128903113af214
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-09-12 12:37:27 +0200

    eventapplication-delete remainder

--- javascript/javascript1/week2/eventapplication.js
+++ javascript/javascript1/week2/eventapplication.js
@@ -1,11 +1,10 @@
 // Event application
 
-function getEventWeekday(day) {
+function getEventWeekday(days) {
     const today = new Date();
     const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const todaysDayAndDate = week[today.getDay()] + ' ' + today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
-    const remainder = day % 7;
-    const eventDay = (today.getDay() + remainder) % 7;
-    return (`Today is ${todaysDayAndDate} and the event is in ${remainder} days. Therefore the event will be held on a ${week[eventDay]}`);
+    const eventDay = (today.getDay() + days % 7) % 7;
+    return (`Today is ${todaysDayAndDate} and the event is in ${days} days. Therefore the event will be held on a ${week[eventDay]}`);
 }
 console.log(getEventWeekday(24));
