221c47f6347a303321506f24b996885a41c60d66
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-09-25 13:32:04 +0200

    week4 homework init commit

---
+++ javascript/javascript1/week3/week4-ex1.js
@@ -0,0 +1,33 @@
+// fizz buzz
+function fizzbuzz(fizz, buzz) {
+const arr = new Array(100);
+for (let i = 0; i < 100; i++) 
+    {
+        arr[i] = i + 1;
+    }
+for (let i = 1; i < arr.length + 1; i++)
+    {
+        if (i % fizz && i % buzz)
+        {
+            console.log(i);
+        }
+        else
+        {
+            if (i % fizz == 0)
+            {
+            console.log('fizz');
+            }
+            if(i % buzz == 0)
+            {
+            console.log('buzz');
+            }
+        }
+    }
+}
+fizzbuzz(4, 12);
+
+//Build a sentiment analyzer
+function getSentimentScore(text){
+    text.toLowerCase().split(' ');
+
+}
