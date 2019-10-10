707728db3295983f52bdc12f44e31c60a3c09a32
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-10-10 13:01:41 +0200

    commit init

---
+++ javascript/javascript2/week2/doubling-of-numbers.js
@@ -0,0 +1,8 @@
+
+//Doubling of numbers
+
+let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+const filterOddNumbers = numbers.filter(number => (newNumbers = number % 2 !== 0)).map(number => (newNumbers = number * 2));
+console.log(filterOddNumbers); // [2, 6]
