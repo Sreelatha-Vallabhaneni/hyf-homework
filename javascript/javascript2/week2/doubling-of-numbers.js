b9581ad91d51b141c5d1985cea0c819b5eec59fe
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-10-30 18:11:29 +0100

    changed variable name

--- javascript/javascript2/week2/doubling-of-numbers.js
+++ javascript/javascript2/week2/doubling-of-numbers.js
@@ -4,5 +4,5 @@
 let numbers = [1, 2, 3, 4];
 let newNumbers = [];
 
-const filterOddNumbers = numbers.filter(number => (newNumbers = number % 2 !== 0)).map(number => (newNumbers = number * 2));
-console.log(filterOddNumbers); // [2, 6]
+const doubledOddNumbers = numbers.filter(number => (newNumbers = number % 2 !== 0)).map(number => (newNumbers = number * 2));
+console.log(doubledOddNumbers); // [2, 6]
