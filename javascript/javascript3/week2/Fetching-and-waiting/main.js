c63742778459b61fe7e2192e596bb846776c980e
Sreelatha Vallabhaneni <sreelatha.vallabhaneni@gmail.com>
2019-11-07 11:15:15 +0100

    rewrite code to async await

--- javascript/javascript3/week2/Fetching-and-waiting/main.js
+++ javascript/javascript3/week2/Fetching-and-waiting/main.js
@@ -2,10 +2,18 @@
 const BASE_URL =
 'https://holidayapi.com/v1/countries?key=daa0c45f-f13c-424f-8727-4ec0f1875368&pretty';
 
-fetch(BASE_URL)
-.then(response => response.json())
-.then((list) => {
-        setTimeout(() => {
+async function fetchAndWait(){
+        const response = await fetch(BASE_URL);
+        const list = await response.json();
+         setTimeout(() => {
             console.log(list);
-            }, 3000)        
-    })
+            }, 3000) 
+} 
+
+try {
+        fetchAndWait();
+} catch (err) {
+        console.log(err);
+        
+}
+
